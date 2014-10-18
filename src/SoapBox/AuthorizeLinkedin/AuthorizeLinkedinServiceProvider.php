56d8d9989f3f4c9337007ee657bc5ba2c75bb692
Victor Mak <victormakcy@gmail.com>
2014-10-18 11:12:18 -0400

    Initial Commit

---
+++ src/SoapBox/AuthorizeLinkedin/AuthorizeLinkedinServiceProvider.php
@@ -0,0 +1,55 @@
+<?php namespace SoapBox\AuthorizeLinkedin;
+
+use Illuminate\Support\ServiceProvider;
+use SoapBox\Authorize\StrategyFactory;
+
+class AuthorizeLinkedinServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	// /**
+	//  * Bootstrap the application events.
+	//  *
+	//  * @return void
+	//  */
+	// public function boot()
+	// {
+	// 	$this->package('soapbox/authorize-linkedin');
+	// }
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		StrategyFactory::register('linkedin', 'SoapBox\AuthorizeLinkedin\LinkedinStrategy');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
